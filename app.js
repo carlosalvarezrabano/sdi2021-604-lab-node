215edeaa3da9014d2cd7636e7c7dbd21959e7254
carlosalvarezrabano <carlos.arov@hotmail.com>
2021-04-19 11:28:34 +0200

    SDI-2021-604-10.1-SW API REST CRUD Canciones.

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, 
 require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig);
+require("./routes/rapicanciones.js")(app, gestorBD);
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
