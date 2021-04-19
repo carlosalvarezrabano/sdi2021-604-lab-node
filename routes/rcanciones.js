002186b11a998ec2fad4947c58aea28f8ee2b59d
carlosalvarezrabano <carlos.arov@hotmail.com>
2021-04-19 12:19:19 +0200

    SDI-2021-604-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -60,13 +60,28 @@ module.exports = function(app, swig, gestorBD) {
                        res.redirect("/errors?mensaje=Error al recuperar los comentarios&tipoMensaje=alert-danger");
                    } else {
                        usuarioPuedeComprarCancion(req.session.usuario, criterio, function(comprar) {
-                           let respuesta = swig.renderFile('views/bcancion.html',
-                               {
-                                   cancion : canciones[0],
-                                   comentarios : comentarios,
-                                   comprar : comprar
-                               });
-                           res.send(respuesta);
+                           let configuracion = {
+                               url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                               method: "get",
+                               headers: {
+                                   "token": "ejemplo",
+                               }
+                           }
+                           let rest = app.get("rest");
+                           rest(configuracion, function (error, response, body) {
+                               console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                               let objetoRespuesta = JSON.parse(body);
+                               let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                               // nuevo campo "usd"
+                               canciones[0].usd = cambioUSD * canciones[0].precio;
+                               let respuesta = swig.renderFile('views/bcancion.html',
+                                   {
+                                       cancion: canciones[0],
+                                       comentarios : comentarios,
+                                       comprar : comprar
+                                   });
+                               res.send(respuesta);
+                           })
                        });
                    }
                 });
