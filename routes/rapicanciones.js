2c3813ee2bc60182eb282256c205e0b3049a2853
carlosalvarezrabano <carlos.arov@hotmail.com>
2021-04-19 15:00:25 +0200

    SDI–604-sesión10-Ejercicio-complementario1

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -21,20 +21,29 @@ module.exports = function(app, gestorBD) {
             precio : req.body.precio,
         }
         // ¿Validar nombre, genero, precio?
-
-        gestorBD.insertarCancion(cancion, function(id){
-            if (id == null) {
-                res.status(500);
-                res.json({
-                    error : "se ha producido un error"
-                })
-            } else {
-                res.status(201);
-                res.json({
-                    mensaje : "canción insertada",
-                    _id : id
-                })
-            }
+        validaDatosAgregarFuncion(cancion, function(errors) {
+           if(errors != null || errors.length > 0) {
+               res.status(500);
+               res.json({
+                   errors : errors
+               })
+           }
+           else {
+               gestorBD.insertarCancion(cancion, function(id){
+                   if (id == null) {
+                       res.status(500);
+                       res.json({
+                           error : "se ha producido un error"
+                       })
+                   } else {
+                       res.status(201);
+                       res.json({
+                           mensaje : "canción insertada",
+                           _id : id
+                       })
+                   }
+               });
+           }
         });
 
     });
@@ -58,17 +67,28 @@ module.exports = function(app, gestorBD) {
     app.delete("/api/cancion/:id", function(req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
 
-        gestorBD.eliminarCancion(criterio,function(canciones){
-            if ( canciones == null ){
+        usuarioEsAutor(req.session.usuario, criterio, function(usuario) {
+            if(usuario == 'undefined' || usuario == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error : "El usuario actual no puede eliminar esta cancion"
                 })
-            } else {
-                res.status(200);
-                res.send( JSON.stringify(canciones) );
             }
-        });
+            else {
+                gestorBD.eliminarCancion(criterio,function(canciones){
+                    if ( canciones == null ){
+                        res.status(500);
+                        res.json({
+                            error : "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.send( JSON.stringify(canciones) );
+                    }
+                });
+            }
+        })
+
     });
 
     app.put("/api/cancion/:id", function(req, res) {
@@ -82,20 +102,32 @@ module.exports = function(app, gestorBD) {
             cancion.genero = req.body.genero;
         if ( req.body.precio != null)
             cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
-            if (result == null) {
+
+        validaDatosActualizarFuncion(cancion, function(errors) {
+            if(errors.length > 0 || errors != null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
-                })
-            } else {
-                res.status(200);
-                res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
+                    errors : errors
                 })
             }
-        });
+            else {
+                gestorBD.modificarCancion(criterio, cancion, function(result) {
+                    if (result == null) {
+                        res.status(500);
+                        res.json({
+                            error : "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.json({
+                            mensaje : "canción modificada",
+                            _id : req.params.id
+                        })
+                    }
+                });
+            }
+        })
+
     });
 
     app.post("/api/autenticar", function(req, res) {
@@ -127,4 +159,55 @@ module.exports = function(app, gestorBD) {
             }
         });
     });
+
+    //Funciones auxiliares
+    function validaDatosAgregarFuncion(cancion, functionCallback) {
+        let errors = new Array();
+        if(cancion.nombre == 'undefined' || cancion.nombre == '' || cacnion.nombre == null) {
+            errors.push(new Error("El campo nombre no puede estar vacío"));
+        }
+        if(cancion.genero == 'undefined' || cancion.genero == '' || cacnion.genero == null) {
+            errors.push(new Error("El campo genero no puede estar vacío"));
+        }
+        if(typeof cancion.precio == 'undefined' || cancion.precio == '' || cacnion.precio == null ||
+            cancion.precio < 0) {
+            errors.push(new Error("El campo precio no puede estar vacío o tener valores negativos"));
+        }
+        functionCallback(errors);
+    }
+
+    function validaDatosActualizarFuncion(cancion, functionCallback) {
+        usuarioEsAutor(req.session.usuario, cancion._id, function(usuario) {
+            let errors = new Array();
+            if(usuario != null) {
+                if(cancion.nombre == 'undefined' || cancion.nombre == '' || cacnion.nombre == null) {
+                    errors.push(new Error("El campo nombre no puede estar vacío"));
+                }
+                if(cancion.genero == 'undefined' || cancion.genero == '' || cacnion.genero == null) {
+                    errors.push(new Error("El campo genero no puede estar vacío"));
+                }
+                if(cancion.precio == 'undefined' || cancion.precio == '' || typeof cacnion.precio == null
+                    || cancion.precio < 0) {
+                    errors.push(new Error("El campo precio no puede estar vacío o tener valores negativos"));
+                }
+            }
+            else {
+                errors.push(new Error("Este usuario no puede modificar esta cancion"));
+            }
+            functionCallback(errors);
+        })
+
+    }
+
+    function usuarioEsAutor(usuario, idCancion, functionCallback) {
+        let criterio_micancion = { $and: [{"_id" : cancionId}, {"autor" : usuario}] };
+        gestorBD.obtenerCanciones(criterio_micancion, function(canciones) {
+            if(canciones.length > 0 && canciones != null) {
+                functionCallback(true);
+            }
+            else {
+                functionCallback(false);
+            }
+        })
+    }
 }
